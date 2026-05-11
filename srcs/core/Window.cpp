d2c96d629e06f327b7b097a52a61cdfee00227ec
Quentin <quentin@host.dynamic.voo.be>
2026-05-11 18:37:45 +0200

    capture mouse

--- srcs/core/Window.cpp
+++ srcs/core/Window.cpp
@@ -23,7 +23,7 @@ Window::Window(Application* app)
 		throw GLWF_WindowError();
 
 	glfwMakeContextCurrent(_window);
-	glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL); // ICI
+	glfwSetInputMode(_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetWindowUserPointer(_window, app);
 
     glfwMakeContextCurrent(_window);
